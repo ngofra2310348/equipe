47cf29c3279fcac42535011f0e49f227a387a152
Franck Trésor Ngoupeyou <2310348@etu.cegepjonquiere.ca>
2024-04-03 12:47:54 -0400

    lab 5 terminé

---
+++ Console_vide/FileName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Colorful;
+using System.Threading.Tasks;
+using Console = Colorful.Console;
+
+namespace Console_vide
+{
+    internal class FileName
+    {
+        public FileName()
+        {
+           
+            Console.WriteAscii("JE T'AIMe");
+        }
+    }
+}
